c4f9d5c832a45162148114bf4bd5b51a96465123
Alessandro <alessandropcline@gmail.com>
2026-03-15 23:34:48 +0100

    php artisan route:cache

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 // Detalle de un post
-Route::get('/posts/{slug}', [PostController::class, 'show'])->name('posts.show');
 
 Route::get('/{id}/{slug}', [PostController::class, 'show'])
     ->name('posts.show');
