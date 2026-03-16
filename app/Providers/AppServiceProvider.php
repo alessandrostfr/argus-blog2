f830d0e041f336b79fd79c5f1a78fae2016209f4
Alessandro <alessandropcline@gmail.com>
2026-03-16 01:12:59 +0100

    correccion de rutas para produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -22,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Carbon::setLocale(config('app.locale'));
     }
 }
